bcfad6f698e4d8defa010634dc5f57067a249126
Kutsan Kaplan <me@kutsankaplan.com>
2021-10-24 18:59:23 +0300

    refactor!: convert CJS modules to ESM

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export declare const name = "Kutsan Kaplan";
+export declare const email = "me@kutsankaplan.com";
+export declare const website = "https://kutsankaplan.com";
--- src/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: 'Kutsan Kaplan',
-  email: 'me@kutsankaplan.com',
-  website: 'https://kutsankaplan.com'
-}
